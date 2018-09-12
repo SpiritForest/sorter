d4b2e8ad5aba2a7cf5a71b5c15ab965ffa50a6bc
SpiritForest <dzahar@mail.ru>
2018-09-12 20:49:39 +0300

    completed sorter

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 class Sorter {
-
-  constructor(age) {
-      this.storage = [];
-      this.age = age;
+  constructor() {
+    this.repository = [];
+    this.fn = (a, b) => a - b;
   }
 
-  add(element) {
-   this.storage[this.storage.length] = element;
-  }
+  add(element) {this.repository.push(element);}
 
-  at(index) {
-      return this.storage[index];
-  }
+  at(index) {return this.repository[index];}
 
-  get length() {
-    return this.storage.length;
-  }
+  get length() {return this.repository.length;}
 
-  toArray() {
-    return this.storage;
-  }
+  toArray() {return this.repository;}
 
   sort(indices) {
-      var mass = [];
-      indices = indices.sort();
-    for (var i = 0; i < indices.length; i++){
-       mass[i] = this.storage[indices[i]];
-    }
-    mass.sort(function (a,b) {return a-b});
-    for (var y = 0; y < indices.length; y++){
-        this.storage[indices[y]] = mass[y];
-    }
-    return this.storage;
+    indices.sort();
+    var sarr = [];
+        if (indices.length > 2 || indices.length == 1){
+          for (let i = indices[0]; i <= indices[indices.length-1]; i++){
+            sarr.push(this.repository[i]);
+          }
+          sarr.sort(this.fn);
+          this.repository.splice(indices[0], indices.length, ...sarr);
+          return this.repository;
+        }
+
+        if (indices.length == 2) {
+          sarr.push(this.repository[indices[0]]);
+          sarr.push(this.repository[indices[1]]);
+          sarr.sort(this.fn);
+          this.repository.splice(indices[0],1,sarr[0]);
+          this.repository.splice(indices[1],1,sarr[1]);
+          return this.repository;
+        }
   }
 
   setComparator(compareFunction) {
-  
+    this.fn = compareFunction;
   }
 }
 
